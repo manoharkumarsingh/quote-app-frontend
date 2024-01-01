c34a1ae7dbd46249b6174425c2135d36ffe871ed
Manohar Singh <manohar.singh@communitybrands.com>
2024-01-01 20:16:46 +0530

    ui fixes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 const client = new ApolloClient({
-  uri: "/graphql",
+  uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
   headers: {
     authorization: localStorage.getItem("token") || "",
